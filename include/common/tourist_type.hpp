342e43337861a95b46e6003ba701592781226455
Jakub Nahacz <jakubnahacz@gmail.com>
2025-11-25 21:38:42 +0100

    feat: add tourist type enum

---
+++ include/common/tourist_type.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+/**
+ * Type of tourist using the ropeway
+ */
+enum class TouristType {
+    PEDESTRIAN,  // occupies 1 slot on a chair
+    CYCLIST      // occupies 2 slots on a chair
+};
