dae21ced874c75f9bb7bce6a5ca1b6f325c11af6
Jakub Nahacz <jakubnahacz@gmail.com>
2025-11-30 19:42:48 +0100

    feat: add tourist type enum

---
+++ include/common/tourist_type.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/**
+ * Type of tourist using the ropeway
+ */
+enum class TouristType {
+    PEDESTRIAN,  // occupies 1 slot on chair
+    CYCLIST      // occupies 2 slots on chair
+};
